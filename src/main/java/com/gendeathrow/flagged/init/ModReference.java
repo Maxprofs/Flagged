699fb4fdc0fa0006e5b1a103688ce1005e74512d
GenDeathrow <gendeathrow@gmail.com>
2017-10-22 20:30:49 -0500

    Fixed animation to wave in the correct direction.. reversed it

--- src/main/java/com/gendeathrow/flagged/init/ModReference.java
+++ src/main/java/com/gendeathrow/flagged/init/ModReference.java
@@ -2,7 +2,7 @@ package com.gendeathrow.flagged.init;
 
 public class ModReference {
 	public static final String MOD_ID = "flagged";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
     public static final String NAME = "Flagged";
     public static final String PROXY_PATH = "com.gendeathrow.flagged.proxy";
     public static final String CHANNEL_NAME = "flaggedgen";
